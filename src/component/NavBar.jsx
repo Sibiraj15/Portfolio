2dae0e9663e137bd726559dd74c0488d2e593493
Sibiraj15 <ssibiraj64@gmail.com>
2025-02-19 11:12:45 +0530

    fix: portfolio

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -11,25 +11,25 @@ import { FaSquareXTwitter } from "react-icons/fa6";
             </a>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <a href="https:/www.linkedin.com/in/your-linkedin-profile"
+            <a href="https://www.linkedin.com/in/sibiraj05/"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Linkedin">
                 <FaLinkedin />
             </a>
-            <a href="https:/www.github.com/in/your-github-profile"
+            <a href="https://github.com/Sibiraj15"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="GitHub">
                 <FaGithub />
             </a>
-            <a href="https:/www.instagram.com/in/your-instagram-profile"
+            <a href="https://www.instagram.com/frnds_cl_me_idiot_/"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Instagram">
                 <FaInstagram />
             </a>
-            <a href="https:/www.twitter.com/in/your-twitter-profile"
+            <a href="https://x.com/s_strangercb"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Twitter">
